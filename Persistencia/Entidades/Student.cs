1f3f35a7eaf6b74a9ff2e2a72521e2fd38847079
Vinicius Jaggi <Vinicius.Jaggi@edu.pucrs.br>
2022-09-26 20:17:11 -0300

    Migration criada, mais parte feita durante a aula(criando modelo de dados complexo

--- Persistencia/Entidades/Student.cs
+++ Persistencia/Entidades/Student.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Persistencia.Entidades
 {
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
